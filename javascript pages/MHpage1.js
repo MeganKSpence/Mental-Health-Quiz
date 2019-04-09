a0d39a43eb9b86b2bc62652dd16c96a03b0cad49
MeganKSpence <43014740+MeganKSpence@users.noreply.github.com>
2019-04-09 16:52:51 -0600

    update

---
+++ javascript pages/MHpage1.js
@@ -0,0 +1,18 @@
+console.log("Welcome to MHpage1 Java!");
+var total = 0;
+var ad3 = 0;
+var ad1 = 0;
+
+function add1() {
+  ad1 = ad1+1;
+  total = ad1;
+  console.log("Yep!")
+  console.log(total);
+}
+
+function add3() {
+   ad3 = ad3+3;
+   total = ad3;
+   console.log("Yay!");
+   console.log(total);
+}
